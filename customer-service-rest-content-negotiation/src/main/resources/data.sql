9e822716bbda49490d468125b85308f7ac839d88
sparsons <sparsons@salesforce.com>
2021-10-30 12:36:55 -0400

    mid-class commit

---
+++ customer-service-rest-content-negotiation/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into  customers(name,phone,email) values ('vinodh','1234567','vinodh.mahendra@simplilearn.com');
+insert into  customers(name,phone,email) values ('bhavya','1234567','bhavya@collins.com');
+insert into  customers(name,phone,email) values ('samarth','1234567','samarth.vishnu@hisowncompany.com');
