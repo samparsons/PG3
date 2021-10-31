24975dbffff3bc721be14878c1059acf585a6438
sparsons <sparsons@salesforce.com>
2021-10-31 10:59:35 -0400

    class update

---
+++ kafka-consumer/src/main/java/com/simplilearn/workshop/KafkaConsumerApplication.java
@@ -0,0 +1,19 @@
+package com.simplilearn.workshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.KafkaListener;
+
+@SpringBootApplication
+public class KafkaConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaConsumerApplication.class, args);
+	}
+	
+	@KafkaListener(topics = "test-topic",groupId="consume-01")
+	public void process(String message) {
+		System.out.println(message);
+	}
+
+}
